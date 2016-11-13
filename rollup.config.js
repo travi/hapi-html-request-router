b5454b02fa2f56afb1c72cf1cbc6b250c2edaaf0
Matt Travi <programmer@travi.org>
2016-11-13 01:39:43 -0600

    build(rollup): used rollup to build `main` and `module` bundles

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+    entry: 'src/plugin.js',
+    plugins: [
+        babel({
+            babelrc: false,
+            exclude: ['./node_modules/**'],
+            presets: ['es2015-rollup']
+        })
+    ],
+    targets: [
+        {dest: 'lib/plugin.cjs.js', format: 'cjs'},
+        {dest: 'lib/plugin.es.js', format: 'es'}
+    ]
+};
